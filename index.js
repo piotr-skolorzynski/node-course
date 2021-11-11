2a5727dc9f93de346095d0b0643f51d320c9434f
Piotr <p.skolorzynski@gmail.com>
2021-11-11 20:35:56 +0100

    modify index.js and math-module and package.json add example of using ES module import/export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { add, sub } = require("./math-module"); //commonJS
+import { add, sub } from "./math-module.js";
 
 console.log(add(1, 4));
 console.log(sub(4, 2));
--- math-module.js
+++ math-module.js
@@ -1,5 +1,4 @@
-const add = (a, b) => a + b;
+export const add = (a, b) => a + b;
 
-const sub = (a, b) => a - b;
+export const sub = (a, b) => a - b;
 
-module.exports = { add, sub };
